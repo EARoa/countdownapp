8a95a0c26c0a2205840b6c708ffa3522f880cac9
Efrain Ayllon <jesusayllonroa@gmail.com>
2015-03-27 10:24:38 -0500

    Initial Commit

---
+++ countdown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  countdown
+//
+//  Created by Efrain Ayllon on 3/27/15.
+//  Copyright (c) 2015 Efrain Ayllon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ countdown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  countdown
+//
+//  Created by Efrain Ayllon on 3/27/15.
+//  Copyright (c) 2015 Efrain Ayllon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
